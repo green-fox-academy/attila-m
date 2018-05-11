512ac50006abd53b13a63b4798932d614b850e79
Attila Murvai <at.murvai@gmail.com>
2018-05-11 13:59:33 +0200

    (feat)IWontCheat: exercise added

---
+++ week-01/day-04/java/src/IWontCheatOnTheExams.java
@@ -0,0 +1,12 @@
+public class IWontCheatOnTheExams {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 101; i++) {
+            System.out.println("I wont cheat on the exam.");
+        }
+    }
+}
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
