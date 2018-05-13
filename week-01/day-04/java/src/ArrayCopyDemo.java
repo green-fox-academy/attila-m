31f70cb1ea99671921ac5417c2d6d7a9da000882
Attila Murvai <at.murvai@gmail.com>
2018-05-14 01:34:43 +0200

    (feat)GuessTheNumber: exercise added

---
+++ week-01/day-04/java/src/ArrayCopyDemo.java
@@ -0,0 +1,9 @@
+class ArrayCopyDemo {
+    public static void main(String[] args) {
+        char[] copyFrom = { 'd', 'e', 'c', 'a', 'f', 'f', 'e', 'i', 'n', 'a', 't', 'e', 'd' };
+        char[] copyTo = new char[7];
+
+        System.arraycopy(copyFrom, 3, copyTo, 0, 7);
+        System.out.println(new String(copyTo));
+    }
+}
---
+++ week-01/day-04/java/src/GuessTheNumber.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+
+        int numberStored = 8;
+
+        Scanner guess = new Scanner(System.in);
+        System.out.println("Guess the number.");
+        int userGuess = guess.nextInt();
+
+        if (userGuess == numberStored) {
+            System.out.println("You found the number: " + numberStored);
+        } else {
+            do {
+                if (userGuess < numberStored) {
+                    System.out.println("The stored number is higher.");
+                } else if (userGuess > numberStored) {
+                    System.out.println("The stroed number is lower.");
+                } else {
+                    System.out.println("Try with a number, buddy.");
+                }
+                userGuess = guess.nextInt();
+            } while (userGuess != numberStored);
+            System.out.println("You found the number: " + numberStored);
+        }
+    }
+}
+
+// Write a program that stores a number, and the user has to figure it out.
+// The user can input guesses, after each guess the program would tell one
+// of the following:
+//
+// The stored number is higher
+// The stried number is lower
+// You found the number: 8
