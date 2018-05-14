f63094822feaa42f7cac408996724420226a2051
Attila Murvai <at.murvai@gmail.com>
2018-05-14 14:32:21 +0200

    feat(AppendA): exercise added

--- week-01/day-04/java/src/AppendA.java
+++ week-01/day-04/java/src/AppendA.java
@@ -1,4 +1,19 @@
 public class AppendA {
+
+  public static void main(String[] args) {
+
+    String am = "kuty";
+    System.out.println(appendA(am));
+
+  }
+
+  public static String appendA(String S) {
+    String A = S + "a";
+
+    return A;
+
+  }
+
 }
 
 
