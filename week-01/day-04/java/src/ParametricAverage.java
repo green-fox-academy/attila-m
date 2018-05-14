3c785bacafc515eddc821a98d02218060cfeea87
Attila Murvai <at.murvai@gmail.com>
2018-05-14 13:29:49 +0200

    feat(ParametricAverage): exercise added

--- week-01/day-04/java/src/ParametricAverage.java
+++ week-01/day-04/java/src/ParametricAverage.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
+
 public class ParametricAverage {
   public static void main(String[] args) {
 
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please enter how many numbers you want to use.");
+    int userNumbers = scanner.nextInt();
+
+    int sum = 0;
+    int userNumbers2;
+
+    System.out.println("Enter " + userNumbers + " numbers.");
+
+    for (int i = 0; i < userNumbers; i++) {
+
+      userNumbers2 = scanner.nextInt();
+
+      sum += userNumbers2;
+
+    }
+    int average = sum / userNumbers;
+
+    System.out.print("Sum: " + sum + ", Average: " + average);
   }
 }
-
 
 // Write a program that asks for a number.
 // It would ask this many times to enter an integer,
