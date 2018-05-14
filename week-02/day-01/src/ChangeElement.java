d6d4f459441d322901790253eac094dd940d09fe
Attila Murvai <at.murvai@gmail.com>
2018-05-14 16:54:15 +0200

    feat(ChangeElement): exercise added

---
+++ week-02/day-01/src/ChangeElement.java
@@ -0,0 +1,13 @@
+public class ChangeElement {
+
+  public static void main(String[] args) {
+    int [] s = {1, 2, 3, 8, 5, 6};
+    s[4] = 4;
+    System.out.println(s[4]);
+  }
+}
+
+/* Create an array variable named `s`
+        #   with the following content: `[1, 2, 3, 8, 5, 6]`
+        # - Change the 8 to 4
+        # - Print the fourth element */
