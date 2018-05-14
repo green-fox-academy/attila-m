3380516af1aefb7030bd79728cd243234db4e9c7
Attila Murvai <at.murvai@gmail.com>
2018-05-14 16:47:02 +0200

    feat(CompareLenght): exercise added

---
+++ week-02/day-01/src/CompareLenght.java
@@ -0,0 +1,20 @@
+public class CompareLenght {
+
+  public static void main(String[] args) {
+    int [] p1 = {1, 2, 3};
+    int [] p2 = {4, 5};
+
+    if (p1.length < p2.length) {
+      System.out.println("true");
+
+    }
+
+  }
+
+}
+
+// - Create an array variable named `p1`
+//   with the following content: `[1, 2, 3]`
+// - Create an array variable named `p2`
+//   with the following content: `[4, 5]`
+// - Print if `p2` has more elements than `p1`
