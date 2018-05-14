e94207757c827aa8be3dc62648d87fa3244c464f
Attila Murvai <at.murvai@gmail.com>
2018-05-14 16:49:57 +0200

    feat(SumElements): exercise added

---
+++ week-02/day-01/src/SumElements.java
@@ -0,0 +1,12 @@
+public class SumElements {
+
+  public static void main(String[] args) {
+    int [] r = {54, 23, 66, 12};
+    System.out.println(r[1] + r[2]);
+  }
+
+}
+
+// - Create an array variable named `r`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
