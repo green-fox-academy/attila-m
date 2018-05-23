75d84d622741f508d6b67419679a3f50dc83ac7a
Attila Murvai <at.murvai@gmail.com>
2018-05-23 15:00:09 +0200

    feat(E03Dominoes): WOODCUTTER version added

--- week-03/day-02/E03Dominoes/src/Dominoes.java
+++ week-03/day-02/E03Dominoes/src/Dominoes.java
@@ -9,23 +9,26 @@ public class Dominoes {
     // eg: [2, 4], [4, 3], [3, 5] ...
 
     System.out.println(dominoes);
-    System.out.println(dominoes.get(2));
 
     List<Domino> sortedDominoes = new ArrayList<>();
-    sortedDominoes.add()
+    sortedDominoes.add(dominoes.get(0));
+    sortedDominoes.add(dominoes.get(4));
+    sortedDominoes.add(dominoes.get(1));
+    sortedDominoes.add(dominoes.get(3));
+    sortedDominoes.add(dominoes.get(5));
+    sortedDominoes.add(dominoes.get(2));
 
-
-
+    System.out.println(sortedDominoes);
   }
 
   static List<Domino> initializeDominoes() {
     List<Domino> dominoes = new ArrayList<>();
-    dominoes.add(new Domino(5, 2));
-    dominoes.add(new Domino(4, 6));
-    dominoes.add(new Domino(1, 5));
-    dominoes.add(new Domino(6, 7));
-    dominoes.add(new Domino(2, 4));
-    dominoes.add(new Domino(7, 1));
+    dominoes.add(new Domino(5, 2)); //0
+    dominoes.add(new Domino(4, 6)); //1
+    dominoes.add(new Domino(1, 5)); //2
+    dominoes.add(new Domino(6, 7)); //3
+    dominoes.add(new Domino(2, 4)); //4
+    dominoes.add(new Domino(7, 1)); //5
     return dominoes;
   }
 
