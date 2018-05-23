2e6ec10b16f650a63cb891f185088674e487d7b6
Attila Murvai <at.murvai@gmail.com>
2018-05-23 12:59:57 +0200

    feat(E01PostIt): exercise added

---
+++ week-03/day-02/E01PostIt/src/PostIt.java
@@ -0,0 +1,15 @@
+public class PostIt {
+
+  String backgroundColor;
+  String textOfPostIt;
+  String textColor;
+
+  public PostIt (String backgroundColor, String textOfPostIt, String textColor) {
+
+    this.backgroundColor = backgroundColor;
+    this.textOfPostIt = textOfPostIt;
+    this.textColor = textColor;
+
+  }
+
+}
---
+++ week-03/day-02/E01PostIt/src/PostItApp.java
@@ -0,0 +1,11 @@
+public class PostItApp {
+
+  public static void main(String[] args) {
+
+  PostIt postIt1 = new PostIt("orange", "Idea 1", "blue");
+  PostIt postIt2 = new PostIt("pink", "Awesome!", "black");
+  PostIt postIt3 = new PostIt("yellow", "Superb!", "Green");
+
+  }
+
+}
