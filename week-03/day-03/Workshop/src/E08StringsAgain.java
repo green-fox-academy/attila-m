37c331fda32bf44ff2cc8271f3035eb515751002
Attila Murvai <at.murvai@gmail.com>
2018-05-24 16:43:50 +0200

    feat(E09StringAgainAndAgain): exercise added

--- week-03/day-03/Workshop/src/E08StringsAgain.java
+++ week-03/day-03/Workshop/src/E08StringsAgain.java
@@ -1,18 +1,18 @@
 public class E08StringsAgain {
   public static void main(String[] args) {
-    recursiveStrings("xasdXxxa","",-1);
+    recursiveStringsAgain("xasdXxxa","",-1);
   }
 
-  private static String recursiveStrings(String xString,String xLessString, int counter) {
+  private static String recursiveStringsAgain(String xString,String xLessString, int counter) {
     counter++;
     if (counter<xString.length()) {
       Character currentChar = xString.charAt(counter);
       if (currentChar.equals('x') ) {
         xLessString += "";
-        return recursiveStrings(xString,xLessString,counter);
+        return recursiveStringsAgain(xString,xLessString,counter);
       } else {
         xLessString += currentChar;
-        return recursiveStrings(xString,xLessString,counter);
+        return recursiveStringsAgain(xString,xLessString,counter);
       }
     } else {
       System.out.println(xLessString);
---
+++ week-03/day-03/Workshop/src/E09StringsAgainAndAgain.java
@@ -0,0 +1,18 @@
+public class E09StringsAgainAndAgain {
+  public static void main(String[] args) {
+    recursiveStringsAgainAndAgain("FunnyWord", "", 0);
+  }
+
+  private static String recursiveStringsAgainAndAgain(String starless, String starful, int counter) {
+    Character currentChar = starless.charAt(counter);
+    if (counter<starless.length()-1) {
+      starful+=currentChar+"*";
+      counter++;
+      return recursiveStringsAgainAndAgain(starless,starful,counter);
+    } else {
+      starful+=starless.charAt(counter);
+      System.out.println(starful);
+      return starful;
+    }
+  }
+}
