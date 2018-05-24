1a3e9352c734eb9dd50dc6c3cc1c6d565a77b0df
Attila Murvai <at.murvai@gmail.com>
2018-05-24 13:41:12 +0200

    feat(E03SumDigit): exercise solved correctly+bonus

--- week-03/day-03/Workshop/src/E03SumDigit.java
+++ week-03/day-03/Workshop/src/E03SumDigit.java
@@ -1,16 +1,31 @@
 public class E03SumDigit {
 
   public static void main(String[] args) {
-    recursiveSumDigit(10100100, 1);
+
+    recursiveSumDigit(123456789, 0);
+    recursiveSumDigitBonus(10001, 0);
   }
 
-  private static int recursiveSumDigit(int num, int counter) {
+  private static int recursiveSumDigit(int number, int sum) {
+
+    if (number>0) {
+      sum+=number%10;
+      number=number/10;
+      return recursiveSumDigit(number,sum);
+    } else {
+      sum+=number;
+      System.out.println(sum);
+      return sum;
+    }
+  }
+
+  private static int recursiveSumDigitBonus(int num, int counter) { // How many digits are there in my number? - bonus exercise
 
     counter++;
 
-    if (num>10) {
+    if (num>=10) {
       num=num/10;
-      return recursiveSumDigit(num, counter);
+      return recursiveSumDigitBonus(num, counter);
     } else {
       System.out.println(counter);
       return counter;
---
+++ week-03/day-03/Workshop/src/E04Power.java
@@ -0,0 +1,12 @@
+public class E04Power {
+  public static void main(String[] args) {
+   // recursivePower(3,2);
+  }
+
+ // private static int recursivePower(int base, int exponent) {
+
+
+
+
+ // }
+}
