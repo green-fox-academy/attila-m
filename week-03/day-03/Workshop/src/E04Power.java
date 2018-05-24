6cafad820c30eecd8a749772a4d80f20c69341ce
Attila Murvai <at.murvai@gmail.com>
2018-05-24 13:58:01 +0200

    feat(E04Power): exercise added

--- week-03/day-03/Workshop/src/E04Power.java
+++ week-03/day-03/Workshop/src/E04Power.java
@@ -1,12 +1,17 @@
 public class E04Power {
   public static void main(String[] args) {
-   // recursivePower(3,2);
+    recursivePower(3,4, 1);
   }
 
- // private static int recursivePower(int base, int exponent) {
-
-
-
-
- // }
+    private static int recursivePower(int base, int exponent, int sum) {
+    if (exponent > 0) {
+      exponent--;
+      sum*=base;
+      System.out.println(sum);
+      return recursivePower(base,exponent,sum);
+    } else {
+      System.out.println(sum);
+      return sum;
+    }
+  }
 }
