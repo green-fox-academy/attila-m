6b32c2cdbb60a619fd3cc300826d82250e74d4ac
Attila Murvai <at.murvai@gmail.com>
2018-05-31 14:24:20 +0200

    feat(E02Sum): add 0element and 1element test

--- week-04/day-04/Workshop/E02Sum/test/SumFunctionTest.java
+++ week-04/day-04/Workshop/E02Sum/test/SumFunctionTest.java
@@ -18,10 +18,10 @@ class SumFunctionTest extends SumFunction {
     }
 
   @Test
-  public void should_returnSum_when_filleddListInput() {
+  public void should_returnSum_when_filledListInput() {
 
     // Assignment
-    int expectedresult1 = 6;
+    int expectedResult1 = 6;
     inputList.add(1);
     inputList.add(2);
     inputList.add(3);
@@ -30,11 +30,37 @@ class SumFunctionTest extends SumFunction {
     int result1 = sumFunction.getSum(inputList);
 
     // Assessment
-    assertEquals(expectedresult1, result1);
+    assertEquals(expectedResult1, result1);
 
   }
 
   @Test
-  public void should_return
+  public void should_return0_when_noInput() {
+
+    // Assignment
+    int expectedResult1 = 0;
+
+    // Act
+    int result1 = sumFunction.getSum(inputList);
+
+    // Assessment
+    assertEquals(expectedResult1, result1);
+
+  }
+
+  @Test
+  public void should_returnSum_whenOneInput() {
+
+    // Assignment
+    int expectedResult1 = 1;
+    inputList.add(1);
+
+    // Act
+    int result1 = sumFunction.getSum(inputList);
+
+    // Assessment
+    assertEquals(expectedResult1, result1);
+
+  }
 
 }
