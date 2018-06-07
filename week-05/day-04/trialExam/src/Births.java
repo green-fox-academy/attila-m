4ca24f38b155f97fcd5bca59c771bc735e14c20c
Attila Murvai <at.murvai@gmail.com>
2018-06-07 12:32:36 +0200

    init(Births): Init exercise

---
+++ week-05/day-04/trialExam/src/Births.java
@@ -0,0 +1,13 @@
+public class Births {
+  public static void main(String[] args) {
+
+  }
+}
+
+// Create a function that
+// - takes a filename as a parameter,
+// - reads a csv file which rows contains data in the following format: <person name>;<birthdate in YYYY-MM-DD format>;<city name>
+// - returns the year when the most births happend.
+
+// You can find such a csv file in this directory: births.csv
+// If you pass births.csv to your function, the result should be 2006.
