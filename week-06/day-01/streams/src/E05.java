aa3d9c7eade10725fb1f0b15aeb415c7eb588414
Attila Murvai <at.murvai@gmail.com>
2018-06-18 15:16:44 +0200

    feat(Stream): Add E05

--- week-06/day-01/streams/src/E05.java
+++ week-06/day-01/streams/src/E05.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+
 public class E05 {
   public static void main(String[] args) {
-    
+    System.out.println(getUpperCase("rAdIoHEad"));
   }
+
+  private static String getUpperCase(String wordToCheck) {
+    String[] chars = wordToCheck.split("");
+    return Arrays.stream(chars)
+            .map(c -> c.charAt(0))
+            .filter(Character::isUpperCase)
+            .map(c -> Character.toString(c))
+            .reduce((uppercase, letter) -> uppercase + letter)
+            .get();
+  }
+
 }
