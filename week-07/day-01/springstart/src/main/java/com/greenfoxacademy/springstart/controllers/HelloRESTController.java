18028e63674d12fc000cf9aba90a83ce4fe93a6e
Attila Murvai <at.murvai@gmail.com>
2018-06-25 15:43:57 +0200

    feat(Springstart): Add html part

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -2,14 +2,17 @@ package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class HelloRESTController {
 
+  AtomicLong counter = new AtomicLong();
+
   @RequestMapping
-  public Greeting greeting(@RequestParam("id") long id, @RequestParam("content") String content) {
-    return new Greeting(id, content);
+  public Greeting greeting(@RequestParam(value = "content", defaultValue = "World!") String content) {
+    return new Greeting(counter.incrementAndGet(),"Hello " + content + "!");
   }
 }
