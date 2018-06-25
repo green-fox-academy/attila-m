ad033f182f3a8d38dd83672660df394a1a73308a
Attila Murvai <at.murvai@gmail.com>
2018-06-25 17:24:23 +0200

    feat(Springstart): Add html name and counter

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,13 +3,19 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
 
+  AtomicLong counter = new AtomicLong();
+
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(Model model, @RequestParam("name") String name) {
+    model.addAttribute("name", name);
+    model.addAttribute("counter", counter.incrementAndGet());
     return "greeting";
   }
 }
