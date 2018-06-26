9fe4653f70c2d2e2dd2eaac5f314a090742d2629
Attila Murvai <at.murvai@gmail.com>
2018-06-26 11:16:26 +0200

    init(BankOfSimba): Init project

---
+++ week-07/day-02/bankofsimba/src/main/java/com/bankofsimba/bankofsimba/BankofsimbaApplication.java
@@ -0,0 +1,12 @@
+package com.bankofsimba.bankofsimba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankofsimbaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BankofsimbaApplication.class, args);
+  }
+}
---
+++ week-07/day-02/bankofsimba/src/main/java/com/bankofsimba/bankofsimba/models/BankAccount.java
@@ -0,0 +1,40 @@
+package com.bankofsimba.bankofsimba.models;
+
+public class BankAccount {
+  private String name;
+  private int balance;
+  private String animalType;
+
+  public BankAccount() {
+  }
+
+  public BankAccount(String name, int balance, String animalType) {
+    this.name = name;
+    this.balance = balance;
+    this.animalType = animalType;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getBalance() {
+    return balance;
+  }
+
+  public void setBalance(int balance) {
+    this.balance = balance;
+  }
+
+  public String getAnimalType() {
+    return animalType;
+  }
+
+  public void setAnimalType(String animalType) {
+    this.animalType = animalType;
+  }
+}
