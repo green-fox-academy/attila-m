2a05712dd2250cc4c267dd7e41de071cd3992402
Attila Murvai <at.murvai@gmail.com>
2018-06-27 13:55:29 +0200

    refactor(FolderStructure): Refactor folder structure

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWorldController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWorldController.java
@@ -1,4 +1,23 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
 public class HelloWorldController {
+
+  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+          "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+          "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+          "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+  String hello = hellos[(int) (Math.random() * hellos.length)];
+
+  @RequestMapping("/web/hellos")
+  public String greeting(Model model) {
+    model.addAttribute("hellos", hello);
+    return "hello_world";
+  }
 }
