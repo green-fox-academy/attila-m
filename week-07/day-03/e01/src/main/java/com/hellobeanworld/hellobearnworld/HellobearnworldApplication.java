a591602a06440ab21cc3d794aa71c7ae6ace41f8
Attila Murvai <at.murvai@gmail.com>
2018-06-27 13:04:21 +0200

    init(HelloBeanWorld): Init project

---
+++ week-07/day-03/e01/src/main/java/com/hellobeanworld/hellobearnworld/HellobearnworldApplication.java
@@ -0,0 +1,12 @@
+package com.hellobeanworld.hellobearnworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobearnworldApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HellobearnworldApplication.class, args);
+  }
+}
