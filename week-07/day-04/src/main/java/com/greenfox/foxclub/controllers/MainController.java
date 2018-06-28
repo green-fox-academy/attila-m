cee8aec4e30cd1f1d853fd2e2ad0c7480d5767d4
Attila Murvai <at.murvai@gmail.com>
2018-06-28 14:56:03 +0200

    feat(FoxClub): Add login page

--- week-07/day-04/src/main/java/com/greenfox/foxclub/controllers/MainController.java
+++ week-07/day-04/src/main/java/com/greenfox/foxclub/controllers/MainController.java
@@ -1,13 +1,24 @@
 package com.greenfox.foxclub.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
+  @GetMapping("/login")
+  public String showLoginPage() {
+    return "login";
+  }
+
   @GetMapping("")
-  public String showIndexPage() {
+  public String submitFox(@RequestParam(value = "name", required = false) String name, Model model) {
+    if (name == null) {
+      return "redirect:/login";
+    }
+    model.addAttribute("name", name);
     return "index";
   }
 }
---
+++ week-07/day-04/src/main/java/com/greenfox/foxclub/models/Fox.java
@@ -0,0 +1,21 @@
+package com.greenfox.foxclub.models;
+
+public class Fox {
+
+  private String name;
+
+  public Fox() {
+  }
+
+  public Fox(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
