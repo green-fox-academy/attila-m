589ff784e766216e4324b3e62db87fa3c0b2fff8
Attila Murvai <at.murvai@gmail.com>
2018-06-28 13:12:51 +0200

    init(FoxClub): Init project

---
+++ week-07/day-04/FoxClub/src/main/java/com/greenfox/foxclub/FoxclubApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.foxclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxclubApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FoxclubApplication.class, args);
+  }
+}
---
+++ week-07/day-04/FoxClub/src/test/java/com/greenfox/foxclub/FoxclubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfox.foxclub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoxclubApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
