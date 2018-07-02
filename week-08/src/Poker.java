169910970e87c4f298f3121b8d8f9479b282a2e4
Attila Murvai <at.murvai@gmail.com>
2018-07-02 15:47:17 +0200

    feat(PokerTest): Add 2 tests

--- week-08/src/Poker.java
+++ week-08/src/Poker.java
@@ -6,4 +6,8 @@ public class Poker {
   public static String getHighCard(String cardA, String cardB) {
     return cardB;
   }
+
+  public static String getWinningHand(String[] white, String[] black) {
+    return "White wins!";
+  }
 }
