e7f495f07636c90b0b126d3cb63920734709d39f
Attila Murvai <at.murvai@gmail.com>
2018-07-03 13:35:19 +0200

    init(TodoDatabase): Init project

---
+++ week-08/day-02/src/main/java/com/greenfox/tododatabase/TododatabaseApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.tododatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TododatabaseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TododatabaseApplication.class, args);
+  }
+}
