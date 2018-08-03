a4504615b2c3f8a60dbe781ccdf17269411df789
Attila Murvai <at.murvai@gmail.com>
2018-08-03 16:49:46 +0200

    feat(MovieDB): Add succesfulauth method

--- project-week/3-tokenauthentication/src/main/java/com/greenfox/tokentesting/security/JWTAuthenticationFilter.java
+++ project-week/3-tokenauthentication/src/main/java/com/greenfox/tokentesting/security/JWTAuthenticationFilter.java
@@ -2,10 +2,13 @@ package com.greenfox.tokentesting.security;
 
 import com.auth0.jwt.impl.ClaimsHolder;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -13,6 +16,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import static com.greenfox.tokentesting.security.SecurityConstants.EXPIRATION_TIME;
+import static com.greenfox.tokentesting.security.SecurityConstants.SECRET;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
   private AuthenticationManager authenticationManager;
@@ -32,7 +42,15 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
   }
 
   @Override
-  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-    super.successfulAuthentication(request, response, chain, authResult);
+  protected void successfulAuthentication(HttpServletRequest request,
+                                          HttpServletResponse response,
+                                          FilterChain chain,
+                                          Authentication authResult) throws IOException, ServletException {
+    ZonedDateTime expirationTimeUTC = ZonedDateTime.now(ZoneOffset.UTC).plus(EXPIRATION_TIME, ChronoUnit.MILLIS);
+    String token = Jwts.builder().setSubject(((User)authResult.getPrincipal()).getUsername())
+            .setExpiration(Date.from(expirationTimeUTC.toInstant()))
+            .signWith(SignatureAlgorithm.ES256, SECRET)
+            .compact();
+
   }
 }
